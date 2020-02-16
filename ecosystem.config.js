0d8439a9d4bf34055a0644e84aad94f99013a4a0
Kyllian <kyllian007@gmail.com>
2020-02-16 13:08:07 +0000

    local ecosystem changes

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps : [{
+    name: 'instagram-discord-bot',
+    script: 'index.js',
+	"shutdown_with_message" : true,
+	watch: true,
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'kyllian',
+      host : '172.19.3.20',
+      key  : '/home/kyllian/.ssh/id_rsa',
+      ref  : 'origin/master',
+      repo : 'git@github.com:InstantlyMoist/instagram-discord-bot.git',
+      path : '/home/kyllian/instagram-discord-bot',
+      'post-deploy' : 'pm2 stop instagram-discord-bot && git pull && npm install && pm2 start /home/kyllian/instagram-discord-bot/ecosystem.config.js'
+    }
+  }
+};
